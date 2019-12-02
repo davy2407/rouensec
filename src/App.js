bb7f3dac5ba64c9eb4f8fe8f3585fc64cbfeb2d7
davy2407 <54894522+davy2407@users.noreply.github.com>
2019-12-02 14:34:52 +0100

    Init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import ReactMapGL, {Marker, Popup} from "react-map-gl";
+import * as latlng from "./data/latlongtest.json";
+
 
 function App() {
+const [viewport, setViewport] = useState({
+  latitude : 49.4431,
+  longitude : 1.0993,
+  width : "100vw",
+  height : "100vh",
+  zoom : 10
+});
+
+const [selectedPoint, setSelectedPoint] = useState(null);
+useEffect(() => {
+  const listener = e => {
+    if(e.key === "Escape"){
+      setSelectedPoint(null);
+    }
+  };
+  window.addEventListener("keydown",listener);
+},[]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ReactMapGL {...viewport}
+      mapboxApiAccessToken="pk.eyJ1IjoiZGF2eTI0MDciLCJhIjoiY2szb2N5NXpjMWpibTNucXhqY2hzdnczZSJ9.iCSTSNxMJ4purLDyJht0zA"
+      mapStyle="mapbox://styles/davy2407/ck3odefkl05e11cmj7dxg5wj8"
+      onViewportChange = {(viewport) => {
+        setViewport(viewport);
+      }}
+      >
+        {latlng.markers.map((pointR)=> (
+          <Marker name={pointR.name} latitude={pointR.lat} longitude={pointR.lng}>
+            <button className="marker-btn" onClick= {(e)=> {
+              e.preventDefault();
+              setSelectedPoint(pointR)
+            }}>
+              <img src="/Map_marker.svg" alt="Marker Icon"/>
+
+            </button>
+
+          </Marker>
+        ))}
+
+        {selectedPoint ? (
+          <Popup 
+          latitude={selectedPoint.lat} 
+          longitude={selectedPoint.lng}
+          onClose={() => {
+            setSelectedPoint(null);
+          }}>
+            <div>
+              <h2>{selectedPoint.name}</h2>
+
+            </div>
+
+          </Popup>
+
+        ) : null}
+
+
+
+
+      </ReactMapGL>
+      
     </div>
   );
 }
